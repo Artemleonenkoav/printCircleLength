7353de9fdd04f2f72216aa36400fdb550c9d14f3
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-19 14:40:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        printCircleLength(5);
+    }
+
+    public static void printCircleLength(int radius) {
+        //напишите тут ваш код
+
+        double Pi = 2*3.14*5;
+        System.out.println(Pi);
+
+
+    }
+}
